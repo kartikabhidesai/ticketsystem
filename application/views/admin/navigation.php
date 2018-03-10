3cb54fe305a2496f45a6362e77d1e8c8ba66ffeb
shailesh <shailesh.vanaliya91@gmail.com>
2018-03-10 23:45:44 +0530

    change in invoice html

--- application/views/admin/navigation.php
+++ application/views/admin/navigation.php
@@ -31,12 +31,12 @@
             <li class="<?= $ticket; ?>">
                 <a href="<?= admin_url() . 'tickets'; ?>"><i class="fa fa-ticket"></i> <span class="nav-label">Tickets</span></a>
             </li>
-<!--            <li class="<?= $sale; ?>">
+            <li class="<?= $sale; ?>">
                 <a href=""><i class="fa fa-gear"></i> <span class="nav-label">Sale</span> <span class="fa arrow"></span></a>
                 <ul class="nav nav-second-level">
                     <li class="<?= $invoice,$add,$pay; ?>"><a href="<?= admin_url() . "invoice"; ?>">invoice</a></li>
                 </ul>
-            </li>-->
+            </li>
 
             <li class="<?= $setting; ?>">
                 <a href=""><i class="fa fa-gear"></i> <span class="nav-label">Setting</span> <span class="fa arrow"></span></a>
