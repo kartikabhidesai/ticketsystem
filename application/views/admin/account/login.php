2470c5e92d586a9e969296955bf9f93a28d5dad9
abhishek <abhishekdesai39@gmail.com>
2018-03-21 01:29:04 +0530

    Change text

--- application/views/admin/account/login.php
+++ application/views/admin/account/login.php
@@ -4,7 +4,7 @@
         <div>
             <h1 class="logo-name"><img src="<?php echo IMAGES.'logo.png'; ?>" style="display: block;margin: 0 auto; text-align: center; width: 200px;padding: 10px;"></h1>
         </div>
-        <h3>Welcome to Help Desk</h3>
+        <h3>Welcome To Our Support Help Desk</h3>
         <!--<p>Perfectly designed and precisely prepared admin theme with over 50 pages with extra new web app views.-->
             <!--Continually expanded and constantly improved Inspinia Admin Them (IN+)-->
         </p>
@@ -20,7 +20,7 @@
             <button type="submit" class="btn btn-primary block full-width m-b">Login</button>
 
             <a href="#"><small>Forgot password?</small></a>
-            <p class="text-muted text-center"><small>Do not have an account?</small></p>
+            <p class="text-muted text-center"><small> Contact: <a href="mailto:helpdesk@experttech.ca">helpdesk@experttech.ca</a></small></p>
             <!--<a class="btn btn-sm btn-white btn-block" href="register">Create an account</a>-->
         </form>
         <!--<p class="m-t"> <small>Inspinia we app framework base on Bootstrap 3 &copy; 2014</small> </p>-->
